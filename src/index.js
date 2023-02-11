710917730e21a445f4d2fe8efe7a073ba6f404bb
prasadbiswas14 <prasad.biswas@joshsoftware.com>
2023-02-11 16:09:37 +0530

    App.jsx basic config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Landing from './components/landing';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <Landing />
+    <App />
   </React.StrictMode>
 );
